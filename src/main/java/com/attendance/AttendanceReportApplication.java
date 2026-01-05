3a60b14cf717598f244348819cf83b7c33cae80c
User <ila35538@gmail.com>
2026-01-05 18:54:10 +0300

    init commit

---
+++ src/main/java/com/attendance/AttendanceReportApplication.java
@@ -0,0 +1,13 @@
+package com.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceReportApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AttendanceReportApplication.class, args);
+    }
+}
